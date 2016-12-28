0700f8fb49d4d320181ce7ce667a7a9ab25ba558
gmahi <gullamahi@gmail.com>
2016-12-28 15:07:57 +0000

    Delete implemented

--- webapp/controller/Edit.controller.js
+++ webapp/controller/Edit.controller.js
@@ -1,15 +1,15 @@
 sap.ui.define([
-		"sapui5/demo/restservice/controller/BaseController",
-		"sapui5/demo/restservice/model/formatter",
-		"sapui5/demo/restservice/model/types",
-		"sap/ui/model/json/JSONModel",
-		"sap/m/MessageToast"
-	], function (BaseController, formatter, types, JSONModel, MessageToast) {
+	"sapui5/demo/restservice/controller/BaseController",
+	"sapui5/demo/restservice/model/formatter",
+	"sapui5/demo/restservice/model/types",
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageToast"
+], function(BaseController, formatter, types, JSONModel, MessageToast) {
 	"use strict";
 	return BaseController.extend("sapui5.demo.restservice.controller.Edit", {
 
-        formatter: formatter,
-        types: types,
+		formatter: formatter,
+		types: types,
 		/* =========================================================== */
 		/* lifecycle methods                                           */
 		/* =========================================================== */
@@ -18,14 +18,14 @@ sap.ui.define([
 		 * Called when the worklist controller is instantiated.
 		 * @public
 		 */
-		onInit : function () {
+		onInit: function() {
 			var oRouter, oViewModel;
 			oRouter = this.getRouter();
 			oViewModel = new JSONModel({
 				"createMode": false
 			});
 			this.getView().setModel(oViewModel, "viewModel");
-			
+
 			oRouter.attachRoutePatternMatched(this._onRouteMatched, this);
 		},
 
@@ -37,41 +37,62 @@ sap.ui.define([
 		 * Navigates back to the Master
 		 * @function
 		 */
-		onNavPress : function () {
+		onNavPress: function() {
 			this.myNavBack("master");
 		},
-		
+
 		/**
 		 * Saves changes to the remote service
 		 * @function
 		 */
-		onSave: function(){
+		onSave: function() {
 			var sLocalPath,
-				sUrl = "", 
+				sUrl = "",
 				oRouter = this.getRouter(),
 				sPath = this.getView().getElementBinding().getPath(),
 				oModel = this.getModel(),
 				oObject = oModel.getProperty(sPath),
 				oBundle = this.getResourceBundle();
-			
+
 			//check if we're in edit or createMode
-			if(!this.getModel("viewModel").getProperty("/createMode")){
+			if (!this.getModel("viewModel").getProperty("/createMode")) {
 				//we're not, so we update an existing entry
 				sUrl = "/Suppliers/" + oObject.id;
 				sLocalPath = sPath;
 			} else {
 				sUrl = "/Suppliers";
 			}
-			oModel.attachEventOnce("requestCompleted", function(){
+			oModel.attachEventOnce("requestCompleted", function() {
 				oRouter.navTo("master");
 				this.getModel("viewModel").setProperty("/createMode", false);
 			}, this);
-			oModel.attachEventOnce("requestFailed", function(){
+			oModel.attachEventOnce("requestFailed", function() {
 				MessageToast.show(oBundle.getText("updateFailed"));
 			});
 			oModel.saveEntry(oObject, sUrl, sLocalPath);
 		},
-		
+
+		/*	Deletes entry from remote service
+			 @function 
+			 */
+
+		onDelete: function() {
+
+			var oModel = this.getModel(),
+				sLocalPath = this.getView().getElementBinding().getPath(),
+				oObject = oModel.getProperty(sLocalPath),
+				that = this;
+			oModel.deleteEntry("/Suppliers/" + oObject.id, sLocalPath);
+			oModel.attachEventOnce("requestCompleted", function() {
+				this.getRouter().navTo("master");
+			}, this);
+
+			oModel.attachEventOnce("requestFailed", function() {
+				MessageToast.show(that.getResourceBundle().getText("deleteFailed"));
+			});
+
+		},
+
 		/* =========================================================== */
 		/* internal methods                                            */
 		/* =========================================================== */
@@ -83,12 +104,12 @@ sap.ui.define([
 		 * @param {sap.ui.base.Event} oEvent pattern match event in route 'object'
 		 * @private
 		 */
-		_onRouteMatched : function (oEvent) {
+		_onRouteMatched: function(oEvent) {
 			var oEventData = oEvent.getParameter("arguments");
-			if (oEvent.getParameter("name")==="master"){
-			    return;
+			if (oEvent.getParameter("name") === "master") {
+				return;
 			}
-			if(oEventData && oEventData.id){
+			if (oEventData && oEventData.id) {
 				this.sObjectPath = "/" + oEventData.id;
 			} else {
 				this.getView().getModel("viewModel").setProperty("/createMode", true);
@@ -105,7 +126,7 @@ sap.ui.define([
 		 * @param {string} sObjectPath path to the object to be bound
 		 * @private
 		 */
-		_bindView : function () {
+		_bindView: function() {
 			var oView = this.getView();
 			oView.bindElement(this.sObjectPath);
 		}
--- webapp/model/AppModel.js
+++ webapp/model/AppModel.js
@@ -1,59 +1,85 @@
 sap.ui.define([
-		"sap/ui/model/json/JSONModel"
-	], function (JSONModel) {
+	"sap/ui/model/json/JSONModel"
+], function(JSONModel) {
 	"use strict";
 
 	return JSONModel.extend("sapui5.demo.restservice.model.AppModel", {
-		saveEntry : function(oObject, sUrl, sLocalPath){
+		saveEntry: function(oObject, sUrl, sLocalPath) {
 			var sType,
-			that = this,
-			oData;
+				that = this,
+				oData;
 			// local path indicates whether we are updating an existing object or creating a new one
-			if(sLocalPath){
+			if (sLocalPath) {
 				sType = "PUT";
 			} else {
 				sType = "POST";
 			}
 			oData = JSON.stringify(oObject);
 			jQuery.ajax({
-                    type : sType,
-                    contentType : "application/json",
-                    data: oData,
-                    url : sUrl,
-                    dataType : "json",
-                    success : function() {
-                       //store the new/updated entry in the model
-                       that._updateModel(sLocalPath, oObject);
-                       //call createEntry to reset the dummy property to empty values
-                       that.createEntry("/");
-                       that.fireRequestCompleted();
-                    },
-                    error : function() {
-                        that.fireRequestFailed();
-                    }
+				type: sType,
+				contentType: "application/json",
+				data: oData,
+				url: sUrl,
+				dataType: "json",
+				success: function() {
+					//store the new/updated entry in the model
+					that._updateModel(sLocalPath, oObject);
+					//call createEntry to reset the dummy property to empty values
+					that.createEntry("/");
+					that.fireRequestCompleted();
+				},
+				error: function() {
+					that.fireRequestFailed();
+				}
 			});
 		},
 
-		createEntry : function(sEntityName){
-			this.setProperty(sEntityName + "createEntry",   
-				 {  
-		            "id" : "",
-		            "Name" : "",
-		            "Address" : {  
-		                "Street" : "",
-		                "City" : "",
-		                "State" : "",
-		                "ZipCode" : "",
-		                "Country" : "",
-		                "PhoneNumber" : ""
-		            }
-				 });
+		createEntry: function(sEntityName) {
+			this.setProperty(sEntityName + "createEntry", {
+				"id": "",
+				"Name": "",
+				"Address": {
+					"Street": "",
+					"City": "",
+					"State": "",
+					"ZipCode": "",
+					"Country": "",
+					"PhoneNumber": ""
+				}
+			});
 		},
 
-		_updateModel : function(sLocalPath, data){
-			if (sLocalPath){
+		deleteEntry: function(sUrl, sLocalPath) {
+			var that = this;
+			jQuery.ajax({
+				type: "DELETE",
+				contentType: "application/json",
+				url: sUrl,
+				dataType: "json",
+				async: true,
+				success: function() {
+					//store the new/updated entry in the model
+					that._updateModel(sLocalPath, null, true);
+					that.fireRequestCompleted();
+				},
+				error: function() {
+					that.fireRequestFailed();
+				}
+
+			});
+
+		},
+
+		_updateModel: function(sLocalPath, data, bDelete) {
+			var aData = this.getData();
+			if (sLocalPath && bDelete) {
+				//remove from model
+				aData.splice(sLocalPath.substr(1), 1);
+				this.setData(aData);
+				this.refresh();
+			} else if (sLocalPath) {
 				//store data for an existing object
-				this.setProperty(sLocalPath, data);	
+				this.setProperty(sLocalPath, data);
 			} else {
 				//store new object: get all Data as array from model, push new entry, set data to the model again
 				var aData = this.getData();
